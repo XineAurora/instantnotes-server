95b28cc02a5a4961aca7fc7b0ce8c7fb1031b7b5
XineAurora <qwerty-20010628@yandex.ru>
2023-04-28 10:04:00 +0400

    Update user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,5 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name         string `gorm:"not null;size:255"`
-	PasswordHash string `gorm:"not null"`
+	PasswordHash string `gorm:"not null;size:255"`
+	Email        string `gorm:"not null;unique;"`
 }
