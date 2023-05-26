3b884264c64b324be8c9d2d8977b4e481140631f
XineAurora <qwerty-20010628@yandex.ru>
2023-05-26 23:03:12 +0400

    Fixed json tags

--- internal/models/folder.go
+++ internal/models/folder.go
@@ -3,8 +3,8 @@ package models
 type Folder struct {
 	ID      uint   `gorm:"primarykey" json:"id"`
 	Name    string `gorm:"size:255" json:"name"`
-	UserID  uint   `gorm:"not null" json:"userid"`
+	UserID  uint   `gorm:"not null" json:"userId"`
 	User    User   `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
-	GroupID *uint  `json:"groupid"`
+	GroupID *uint  `json:"groupId"`
 	Group   Group  `gorm:"foreignKey:GroupID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 }
